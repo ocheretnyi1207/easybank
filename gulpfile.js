71d844281d62bd87417790faed8666238c132746
Ocheretnyi Anton <ocheretnyi.spb@gmail.com>
2020-09-10 17:39:24 +0300

    Initial setup & styling mobile version

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const gulp = require("gulp");
+const plumber = require("gulp-plumber");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps")
+const server = require("browser-sync");
+
+gulp.task("css", function () {
+  return gulp.src("src/sass/style.sass")
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("src/css"))
+    .pipe(server.stream())
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "src/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+
+gulp.watch("src/sass/**/*.sass", gulp.series("css"));
+gulp.watch("src/*.html", gulp.series("refresh"));
+gulp.watch("src/*.html").on("change", server.reload);
+
+gulp.task("build", gulp.series("css"));
+gulp.task("start", gulp.series("build", "server"));
