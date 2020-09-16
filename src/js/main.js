3ef6c8f0e0aadd78b27090bcd91cf8d2e79c6639
Ocheretnyi Anton <ocheretnyi.spb@gmail.com>
2020-09-16 03:10:21 +0300

    styling mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -7,17 +7,17 @@ const sectionAboutBank =document.querySelector(".about-bank");
 btnOpenMenu.addEventListener("click", (evt) => {
   evt.preventDefault();
 
-  headerNavMenu.classList.toggle("hide")
+  headerNavMenu.classList.toggle("headernav-list--hide")
 
-  if (!headerNavMenu.classList.contains("hide")) {
+  if (!headerNavMenu.classList.contains("headernav-list--hide")) {
     btnOpenMenu.style.backgroundImage = `url("../img/icon-close.svg")`;
     btnOpenMenu.style.width = "18px";
     btnOpenMenu.style.height = "19px";
-    sectionAboutBank.style.background = `linear-gradient(180deg, #2d314d 0%, rgba(45, 49, 77, 0) 100%), url("../img/bg-about.jpg") no-repeat`;
+    sectionAboutBank.style.background = `linear-gradient(180deg, #2d314d 0%, rgba(45, 49, 77, 0) 100%), url("../img/bg-about.jpg") -41px -83px no-repeat`;
   } else {
     btnOpenMenu.style.backgroundImage = `url("../img/icon-burger.svg")`;
     btnOpenMenu.style.width = "24px";
     btnOpenMenu.style.height = "11px";
-    sectionAboutBank.style.backgroundImage = `url("../img/bg-about.jpg") no-repeat`;
+    sectionAboutBank.style.background = `url("../img/bg-about.jpg") -41px -83px no-repeat`;
   }
 });
