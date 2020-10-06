5f71866eedf2e039fb3cc51b12643ecb1a9b3d72
Anton Ocheretnyi <ocheretnyi.spb@gmail.com>
2020-10-06 17:02:03 +0300

    Deploy projects

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,53 @@
 const gulp = require("gulp");
 const plumber = require("gulp-plumber");
 const sass = require("gulp-sass");
-const sourcemaps = require("gulp-sourcemaps")
+const sourcemaps = require("gulp-sourcemaps");
 const server = require("browser-sync");
 const imagemin = require("gulp-imagemin");
+const rename = require("gulp-rename");
+const csso = require("gulp-csso");
+const del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("src/scss/style.scss")
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("src/css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream())
+});
+
+gulp.task("image", function () {
+  return gulp.src("src/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.mozjpeg({progressive: true}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("build/img/"))
+})
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "src/fonts/**/*.{woff2,woff}",
+    "src/js/**/*.js",
+    "src/index.html"
+  ], {
+    base: "src"
+  })
+  .pipe(gulp.dest("build"))
+});
+
+gulp.task ("clean", function () {
+  return del("build")
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "src/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -30,19 +60,9 @@ gulp.task("refresh", function (done) {
   done();
 });
 
-gulp.task("image", function () {
-  return gulp.src("src/img/**/*.{png,jpg,svg}")
-  .pipe(imagemin([
-    imagemin.optipng({optimizationLevel: 3}),
-    imagemin.mozjpeg({progressive: true}),
-    imagemin.svgo()
-  ]))
-  .pipe(gulp.dest("src/img/"))
-})
-
 gulp.watch("src/scss/**/*.scss", gulp.series("css"));
 gulp.watch("src/*.html", gulp.series("refresh"));
 gulp.watch("src/*.html").on("change", server.reload);
 
-gulp.task("build", gulp.series("css"));
+gulp.task("build", gulp.series("clean", "css", "image", "copy"));
 gulp.task("start", gulp.series("build", "server"));
--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,24 @@
 "use sctrict"
 
-const btnOpenMenu = document.querySelector(".page-header__btn");
-const headerNavMenu = document.querySelector(".headernav-list");
+const headerBtn = document.querySelector(".page-header__btn");
+const navMenu = document.querySelector(".headernav-list");
 const sectionAboutBank = document.querySelector(".about-bank");
+const WIDTH_DSKTOP = 1200;
 
-btnOpenMenu.addEventListener("click", (evt) => {
+headerBtn.addEventListener("click", (evt) => {
   evt.preventDefault();
 
-  headerNavMenu.classList.toggle("headernav-list--hide")
+  navMenu.classList.toggle("headernav-list--hide");
 
-  if (!headerNavMenu.classList.contains("headernav-list--hide")) {
-    btnOpenMenu.style.backgroundImage = `url("../img/icon-close.svg")`;
-    btnOpenMenu.style.width = "18px";
-    btnOpenMenu.style.height = "19px";
+  if (!navMenu.classList.contains("headernav-list--hide")) {
+    headerBtn.style.backgroundImage = `url("../img/icon-close.svg")`;
+    headerBtn.style.width = "18px";
+    headerBtn.style.height = "19px";
     sectionAboutBank.style.background = `linear-gradient(180deg, #2d314d 0%, rgba(45, 49, 77, 0) 100%), url("../img/bg-about.jpg") -41px -83px no-repeat`;
-    headerNavMenu.style.boxShadow = "1px 1px 5px 4px rgba(0,0,0,0.2)";
   } else {
-    btnOpenMenu.style.backgroundImage = `url("../img/icon-burger.svg")`;
-    btnOpenMenu.style.width = "24px";
-    btnOpenMenu.style.height = "11px";
+    headerBtn.style.backgroundImage = `url("../img/icon-burger.svg")`;
+    headerBtn.style.width = "24px";
+    headerBtn.style.height = "11px";
     sectionAboutBank.style.background = `url("../img/bg-about.jpg") -41px -83px no-repeat`;
   }
-});
+})
